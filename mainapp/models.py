3613c8e1bfdb144a32fee936d48d626d4d0b4e1f
sleeping-jazz <jaswanth152007@gmail.com>
2026-03-16 13:33:30 +0530

    1st Jazz

--- mainapp/models.py
+++ mainapp/models.py
@@ -5,6 +5,8 @@ class User(models.Model):
     username = models.CharField(max_length=100)
     email = models.EmailField()
     password = models.CharField(max_length=200)
+    updates = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
 class Media(models.Model):
     MEDIA_TYPES=[('movie','Movie'),('anime','Anime'),('game','Game'),('webseries','Webseries')]
@@ -15,15 +17,22 @@ class Media(models.Model):
     episode_count=models.IntegerField(null=True,blank=True)
     minutes_per_episode=models.IntegerField(null=True,blank=True)
     status=models.CharField(max_length=20)
+    updates = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
 
 class PersonalList(models.Model):
     STATUS=[('planned','Plan'),('watching','Watching'),('completed','Completed'),('dropped','Dropped')]
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     media=models.ForeignKey(Media,on_delete=models.CASCADE)
     status=models.CharField(max_length=20,choices=STATUS)
+    updates = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
 class Discussion(models.Model):
     topic=models.CharField(max_length=200)
     description=models.TextField()
     upvotes=models.IntegerField(default=0)
     downvotes=models.IntegerField(default=0)
+    updates = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
